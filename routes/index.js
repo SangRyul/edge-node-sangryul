a2fd886b79e3b1806e2ad9a3fb9bb1f728661a66
sangryul <ksl970330@naver.com>
2020-10-01 00:42:59 +0900

    add pingpong message and README.md

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ping', function(req, res, next) {
+  let message = "pong"
+  res.send(JSON.stringify(message))
+});
+
 module.exports = router;
